28e5264068bd321ae2ad584d62a96e3582c682b5
Nikita Pedorenko <nikitnya1@gmail.com>
2019-03-18 08:32:02 +0200

    Test

--- LAB4DS/LAB4DS/Program.cs
+++ LAB4DS/LAB4DS/Program.cs
@@ -12,16 +12,16 @@ namespace LAB4DS
         {
             AVLTree tree = new AVLTree();
             
-            tree.Add(10);
-            tree.Add(9);
-            tree.Add(8);
-            tree.Add(7);
-            tree.Add(6);
-            tree.Add(5);
-            tree.Add(4);
             tree.Add(3);
-            tree.Add(2);
+            tree.Add(4);
+            tree.Add(7);
             tree.Add(1);
+            tree.Add(2);
+            tree.Add(5);
+            tree.Add(8);
+            tree.Add(9);
+            tree.Add(11);
+            tree.Add(12);
 
             // Console.WriteLine(tree.top.left.left.data);
 
